0e136bebd54797b767577c346b798bb3df25dcf0
pablo <pablgonz@yahoo.com>
2026-08-01 01:06:27 -0400

    Test workfolwww

--- build.lua
+++ build.lua
@@ -146,9 +146,9 @@ uploadconfig = {
 typesetfiles  = {"spintent.dtx"}
 
 function typeset(file)
-  print("** Running: arara --verbose "..file..".dtx")
+  print("** Running: arara "..file..".dtx")
   local file = jobname(sourcefiledir.."/spintent.dtx")
-  local errorlevel = runcmd("arara --verbose "..file..".dtx", typesetdir, {"TEXINPUTS","LUAINPUTS"})
+  local errorlevel = runcmd("arara "..file..".dtx", typesetdir, {"TEXINPUTS","LUAINPUTS"})
   if errorlevel ~= 0 then
     error("Error!!: Typesetting "..file..".dtx")
     return errorlevel
