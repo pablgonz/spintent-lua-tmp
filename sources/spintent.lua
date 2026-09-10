c8c6ff08aafaafa7e45e8030cde8cb26ef940c40
pablo <pablgonz@yahoo.com>
2026-09-09 23:20:00 -0300

    Primer intento para \sprad y \spdiam

--- sources/spintent.lua
+++ sources/spintent.lua
@@ -1,5 +1,5 @@
 --[[
-     Lua module spintent.lua for spintent package - v0.99 [2026-09-09]
+     Lua module spintent.lua for spintent package - v0.99 [2026-09-10]
 --]]
 
 -- CACHÉ, LPEG Y HERRAMIENTAS GLOBALES
