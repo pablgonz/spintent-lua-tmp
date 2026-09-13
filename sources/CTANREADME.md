579d5887ef5858eec7d293bc3cdfaa5221d47b19
pablo <pablgonz@yahoo.com>
2026-09-13 19:04:57 -0300

    Update order

--- sources/CTANREADME.md
+++ sources/CTANREADME.md
@@ -1,6 +1,6 @@
 ## spintent — Spanish parse intent
 
-Release v0.99 \[2026-09-11\]
+Release v0.99 \[2026-09-13\]
 
 ## Description
 
--- sources/spintent.lua
+++ sources/spintent.lua
@@ -1,5 +1,5 @@
 --[[
-     Lua module spintent.lua for spintent package - v0.99 [2026-09-11]
+     Lua module spintent.lua for spintent package - v0.99 [2026-09-13]
 --]]
 
 -- CACHÉ, LPEG Y HERRAMIENTAS GLOBALES
